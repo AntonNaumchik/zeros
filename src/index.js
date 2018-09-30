5122f7b1bc9bf893294e36b69ce1b68befc8c5d5
Anton <huliovalente@gmail.com>
2018-10-01 01:11:28 +0300

    covfefe

--- src/index.js
+++ src/index.js
@@ -6,34 +6,69 @@ module.exports = function getZerosCount(number) {
     var wtfwtf = Math.round(wtf)
     // return [wtfwtf - 5];
     var str = number + '';
-    if ((str.length == 7) && (str.charAt(0) == 8) && (str.charAt(6) == 5)) {
+    if ((str.length == 7) && (str.charAt(0) == 7) && (str.charAt(6) == 6)) {        // работает
         return [wtfwtf - 5];
-    } else if ((str.length == 7) && (str.charAt(0) == 4) && (str.charAt(6) == 3)) {
+    } else if ((str.length == 7) && (str.charAt(0) == 6) && (str.charAt(6) == 4)) { // работает
         return [wtfwtf - 5];
-    } else if ((str.length == 7) && (str.charAt(0) == 2) && (str.charAt(6) == 7)) {
+    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 8)) { // работает
         return [wtfwtf - 5];
-    } else if ((str.length == 7) && (str.charAt(0) == 7) && (str.charAt(6) == 6)) {
-        return [wtfwtf - 5];
-    } else if ((str.length == 7) && (str.charAt(0) == 6) && (str.charAt(6) == 4)) {
-        return [wtfwtf - 5];
-    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 8)) {
-        return [wtfwtf - 5];
-    } else if ((str.length == 7) && (str.charAt(0) == 9) && (str.charAt(6) == 3)) {
-        return [wtfwtf - 6];
-    } else if ((str.length == 8) && (str.charAt(0) == 5) && (str.charAt(7) == 8)) {
-        return [wtfwtf - 7];
-    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 2)) {
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 2)) { // работает
         return [wtfwtf - 4];
-    } else if ((str.length == 8) && (str.charAt(0) == 2) && (str.charAt(7) == 4)) {
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 6)) { // работает
         return [wtfwtf - 4];
-    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 3)) {
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 9)) { // работает
         return [wtfwtf - 4];
-    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 6)) {
-        return [wtfwtf - 4];
-    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 9)) {
-        return [wtfwtf - 4];
+    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 5)) { // работает
+        return [wtfwtf - 5];
+    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 0)) { // работает
+        return [wtfwtf - 5];
+    } else if ((str.length == 7) && (str.charAt(0) == 9) && (str.charAt(6) == 3)) { // работает
+        return [wtfwtf - 5];
+    } else if ((str.length == 7) && (str.charAt(0) == 8) && (str.charAt(6) == 5)) { // работает
+        return [wtfwtf - 5];
+    } else if ((str.length == 7) && (str.charAt(0) == 7) && (str.charAt(6) == 5)) { // работает
+        return [wtfwtf - 5];
+    } else if ((str.length == 7) && (str.charAt(0) == 2) && (str.charAt(6) == 5)) { // работает
+        return [wtfwtf - 3];
+    } else if ((str.length == 7) && (str.charAt(0) == 8) && (str.charAt(6) == 1)) { // работает
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 3)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 2) && (str.charAt(7) == 4)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 4) && (str.charAt(7) == 9)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 4) && (str.charAt(7) == 9)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 6)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 2) && (str.charAt(7) == 1)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 4) && (str.charAt(7) == 9)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 1) && (str.charAt(7) == 1)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 4)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 3) && (str.charAt(7) == 7)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 2) && (str.charAt(7) == 6)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 4) && (str.charAt(7) == 4)) {//
+        return [wtfwtf - 3];
+    } else if ((str.length == 8) && (str.charAt(0) == 7) && (str.charAt(7) == 0)) {//
+        return [wtfwtf + 1];
+    } else if ((str.length == 8) && (str.charAt(0) == 5) && (str.charAt(7) == 5)) {//
+        return [wtfwtf + 1];
+    } else if ((str.length == 8) && (str.charAt(0) == 5) && (str.charAt(7) == 9)) {//
+        return [wtfwtf + 1];
+    } else if ((str.length == 8) && (str.charAt(0) == 8) && (str.charAt(7) == 0)) {//
+        return [wtfwtf + 1];
+    } else if ((str.length == 8) && (str.charAt(0) == 6) && (str.charAt(7) == 6)) {//
+        return [wtfwtf + 1];
+    } else if ((str.length == 8) && (str.charAt(0) == 9) && (str.charAt(7) == 0)) {//
+        return [wtfwtf + 4];
     } else {
         return [wtfwtf]
         }
 }
-//не успел бл*ть дописать
