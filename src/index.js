7b6d0b839234e9b5883644be20cd4d1a49a9e972
Anton <huliovalente@gmail.com>
2018-09-30 22:46:39 +0300

    ver.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+    var koef = 0.2499999;
+    var wtf = number * koef;
+    var wtfwtf = Math.round(wtf)
+    return [wtfwtf];
+    
 }
